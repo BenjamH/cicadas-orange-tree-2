55bcc64c41884cf8b6755c0fc94a7f2c4357219c
Ben Hsieh <Ben.T.Hsieh@gmail.com>
2015-09-21 17:14:19 -0700

    add orange-tree challenge

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,2 +1,36 @@
 /* Write your code for the plain-old-js-objects-spec here */
+FRUIT_BEARING_AGE = 10;
+MAX_AGE = 60;
 
+var createTree = function(){
+  var tree = {
+    age: 0,
+    height: 0,
+    orangeCount: 0,
+    grow: function(){
+      this.age += 1;
+      this.height += 10;
+      if (this.age >= FRUIT_BEARING_AGE){
+        this.orangeCount += Math.floor((Math.random() * 10) + 1);
+        };
+      if (this.age > MAX_AGE){
+        this.isAlive = false;
+        };
+    },
+    isAlive: true,
+    dropOrange: function(){
+      this.orangeCount -= 1;
+      var orange = {};
+      return orange
+    },
+  }
+  return tree;
+}
+
+
+function pickOrange(){
+  var orange = {
+    diameter: 1
+  }
+  return orange;
+}
